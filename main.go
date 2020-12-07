7b7f92e3dce3fd6f1288f8b97cfeaa7bb6dd324f
rick <rick@jenkins-zh.cn>
2020-12-07 16:58:00 +0800

    Add more alias commands

--- main.go
+++ main.go
@@ -79,12 +79,24 @@ func getDefault() []alias.Alias {
 	}, {
 		Name: "ctl-log", Command: "-n kubesphere-system logs deploy/ks-controller-manager --tail 50 -f",
 	}, {
+		Name: "ctl-reset", Command: `-n kubesphere-system patch deploy ks-controller-manager --type='json' -p='[{"op":"replace","path":"/spec/template/spec/containers/0/image","value":"kubesphere/ks-controller-manager:v3.0.0"}]'`,
+	}, {
+		Name: "ctl-reset-dev", Command: `-n kubesphere-system patch deploy ks-controller-manager --type='json' -p='[{"op":"replace","path":"/spec/template/spec/containers/0/image","value":"kubespheredev/ks-controller-manager:latest"}]'`,
+	}, {
 		Name: "api-log", Command: "-n kubesphere-system logs deploy/ks-apiserver --tail 50 -f",
+	}, {
+		Name: "api-reset", Command: `-n kubesphere-system patch deploy ks-apiserver --type='json' -p='[{"op":"replace","path":"/spec/template/spec/containers/0/image","value":"kubesphere/ks-apiserver:v3.0.0"}]'`,
+	}, {
+		Name: "api-reset-dev", Command: `-n kubesphere-system patch deploy ks-apiserver --type='json' -p='[{"op":"replace","path":"/spec/template/spec/containers/0/image","value":"kubespheredev/ks-apiserver:latest"}]'`,
 	}, {
 		Name: "devops-enable", Command: `-n kubesphere-system patch cc ks-installer -p '{"spec":{"devops":{"enabled":true}}}' --type="merge"`,
 	}, {
 		Name: "devops-disable", Command: `-n kubesphere-system patch cc ks-installer -p '{"spec":{"devops":{"enabled":false}}}' --type="merge"`,
 	}, {
 		Name: "install-log", Command: `-n kubesphere-system logs deploy/ks-installer --tail 50 -f`,
+	}, {
+		Name: "console-reset", Command: `-n kubesphere-system patch deploy ks-console --type='json' -p='[{"op":"replace","path":"/spec/template/spec/containers/0/image","value":"kubesphere/ks-console:v3.0.0"}]'`,
+	}, {
+		Name: "console-reset-dev", Command: `-n kubesphere-system patch deploy ks-console --type='json' -p='[{"op":"replace","path":"/spec/template/spec/containers/0/image","value":"kubespheredev/ks-console:latest"}]'`,
 	}}
 }
