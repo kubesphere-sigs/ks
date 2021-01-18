df4e1ce28236558a8a5023e1b66f56ab665d934f
Zhao Xiaojie <rick@jenkins-zh.cn>
2021-01-18 09:58:25 +0800

    Add support to enable sonarqube (#28)

---
+++ kubectl-plugin/component/log.go
@@ -0,0 +1,114 @@
+package component
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	kstypes "github.com/linuxsuren/ks/kubectl-plugin/types"
+	"github.com/spf13/cobra"
+	"io"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+)
+
+// LogOption is the option for component log command
+type LogOption struct {
+	Option
+
+	Follow bool
+	Tail   int64
+}
+
+// NewComponentLogCmd returns a command to enable (or disable) a component by name
+func NewComponentLogCmd(client dynamic.Interface, clientset *kubernetes.Clientset) (cmd *cobra.Command) {
+	opt := &LogOption{
+		Option: Option{
+			Clientset: clientset,
+			Client:    client,
+		},
+	}
+	cmd = &cobra.Command{
+		Use:     "log",
+		Short:   "Output the log of KubeSphere component",
+		PreRunE: opt.componentNameCheck,
+		RunE:    opt.logRunE,
+	}
+
+	flags := cmd.Flags()
+	flags.StringVarP(&opt.Name, "name", "n", "",
+		"The name of target component which you want to reset.")
+	flags.BoolVarP(&opt.Follow, "follow", "f", true,
+		"Specify if the logs should be streamed.")
+	flags.Int64VarP(&opt.Tail, "tail", "", 50,
+		`Lines of recent log file to display.`)
+	return
+}
+
+func (o *LogOption) logRunE(cmd *cobra.Command, args []string) (err error) {
+	if o.Clientset == nil {
+		err = fmt.Errorf("kubernetes clientset is nil")
+		return
+	}
+
+	ctx := context.TODO()
+	var ns, name string
+	if ns, name = o.getNsAndName(o.Name); name == "" {
+		err = fmt.Errorf("not supported yet: %s", o.Name)
+		return
+	}
+
+	var data []byte
+	buf := bytes.NewBuffer(data)
+	var rawPip *unstructured.Unstructured
+	deploy := &simpleDeploy{}
+	if rawPip, err = o.Client.Resource(kstypes.GetDeploySchema()).Namespace(ns).Get(ctx, name, metav1.GetOptions{}); err == nil {
+		enc := json.NewEncoder(buf)
+		enc.SetIndent("", "    ")
+		if err = enc.Encode(rawPip); err != nil {
+			return
+		}
+
+		cmd.Println(buf)
+		if err = json.Unmarshal(buf.Bytes(), deploy); err != nil {
+			return
+		}
+	}
+
+	var podList *v1.PodList
+	var podName string
+	if podList, err = o.Clientset.CoreV1().Pods(ns).List(ctx, metav1.ListOptions{
+		LabelSelector: labels.SelectorFromSet(deploy.Spec.Selector.MatchLabels).String(),
+	}); err == nil {
+		if len(podList.Items) > 0 {
+			podName = podList.Items[0].Name
+		}
+	} else {
+		return
+	}
+
+	if podName == "" {
+		err = fmt.Errorf("cannot found the pod with deployment '%s'", name)
+		return
+	}
+
+	if len(deploy.Spec.Selector.MatchLabels) > 0 {
+		req := o.Clientset.CoreV1().Pods(ns).GetLogs(podName, &v1.PodLogOptions{
+			Follow:    o.Follow,
+			TailLines: &o.Tail,
+		})
+		var podLogs io.ReadCloser
+		if podLogs, err = req.Stream(context.TODO()); err == nil {
+			defer func() {
+				_ = podLogs.Close()
+			}()
+
+			_, err = io.Copy(cmd.OutOrStdout(), podLogs)
+		}
+	}
+	return
+}
---
+++ kubectl-plugin/component/sonarqube.go
@@ -0,0 +1,75 @@
+package component
+
+import (
+	"context"
+	"fmt"
+	kstypes "github.com/linuxsuren/ks/kubectl-plugin/types"
+	"gopkg.in/yaml.v3"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/dynamic"
+	"strings"
+)
+
+const cfgFile = "local_config.yaml"
+
+func integrateSonarQube(client dynamic.Interface, ns, name, sonarqubeURL, sonarqubeToken string) (err error) {
+	ctx := context.TODO()
+	var rawConfigMap *unstructured.Unstructured
+	if rawConfigMap, err = client.Resource(kstypes.GetConfigMapSchema()).Namespace(ns).
+		Get(ctx, name, metav1.GetOptions{}); err == nil {
+		data := rawConfigMap.Object["data"]
+		dataMap := data.(map[string]interface{})
+		result := updateConsoleConfig(dataMap[cfgFile].(string), sonarqubeURL)
+		if strings.TrimSpace(result) == "" {
+			err = fmt.Errorf("error happend when parse kubesphere-config")
+			return
+		}
+
+		ctx := context.TODO()
+		dataMap[cfgFile] = result
+		rawConfigMap.Object["data"] = dataMap
+		if _, err = client.Resource(kstypes.GetConfigMapSchema()).Namespace(ns).Update(ctx,
+			rawConfigMap, metav1.UpdateOptions{}); err != nil {
+			return
+		}
+
+		patch := fmt.Sprintf(`[{"op": "add", "path": "/spec/devops/sonarqube", "value": {"externalSonarUrl":"%s","externalSonarToken":"%s"}}]`,
+			sonarqubeURL, sonarqubeToken)
+		fmt.Println(patch)
+		if _, err = client.Resource(kstypes.GetClusterConfiguration()).Namespace(ns).Patch(ctx,
+			"ks-installer", types.JSONPatchType,
+			[]byte(patch),
+			metav1.PatchOptions{}); err != nil {
+			return
+		}
+	}
+	return
+}
+
+func updateConsoleConfig(yamlf, sonarqubeURL string) string {
+	mapData := make(map[string]interface{})
+	if err := yaml.Unmarshal([]byte(yamlf), mapData); err == nil {
+		var obj interface{}
+		var ok bool
+		var mapObj map[string]interface{}
+		if obj, ok = mapData["client"]; ok {
+			mapObj = obj.(map[string]interface{})
+		} else {
+			return ""
+		}
+
+		if obj, ok = mapObj["devops"]; ok {
+			mapObj = obj.(map[string]interface{})
+		} else {
+			devopsOpt := make(map[string]interface{}, 1)
+			mapObj["devops"] = devopsOpt
+			mapObj = devopsOpt
+		}
+
+		mapObj["sonarqubeURL"] = sonarqubeURL
+	}
+	resultData, _ := yaml.Marshal(mapData)
+	return string(resultData)
+}
