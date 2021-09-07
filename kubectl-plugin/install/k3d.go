4f73b1051f3309e5bac75d03f786bd98fbe87151
rick <1450685+LinuxSuRen@users.noreply.github.com>
2021-09-07 11:20:28 +0800

    Install kubectl before run k3d

--- kubectl-plugin/install/k3d.go
+++ kubectl-plugin/install/k3d.go
@@ -71,8 +71,9 @@ func (o *k3dOption) preRunE(cmd *cobra.Command, args []string) (err error) {
 		Fetch:    o.fetch,
 	}
 	err = is.CheckDepAndInstall(map[string]string{
-		"k3d":    "rancher/k3d",
-		"docker": "docker",
+		"k3d":     "rancher/k3d",
+		"docker":  "docker",
+		"kubectl": "kubectl",
 	})
 	return
 }
