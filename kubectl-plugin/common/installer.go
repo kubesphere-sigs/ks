54c19bc02fc657ac6cc56de456b883119856c48f
Rick <1450685+LinuxSuRen@users.noreply.github.com>
2021-09-11 10:18:34 +0800

    Print more cluster info when setting multi-cluster (#121)

--- kubectl-plugin/common/installer.go
+++ kubectl-plugin/common/installer.go
@@ -10,17 +10,23 @@ type KSInstallerSpec struct {
 	Version        string
 	ImageNamespace string
 
-	Servicemesh   ComponentStatus
-	Openpitrix    ComponentStatus
-	Notification  ComponentStatus
-	NetworkPolicy ComponentStatus
-	MetricsServer ComponentStatus
-	Logging       ComponentStatus
-	Events        ComponentStatus
-	DevOps        ComponentStatus
-	Auditing      ComponentStatus
-	Alerting      ComponentStatus
-	Multicluster  Multicluster `yaml:"multicluster"`
+	Authentication Authentication `yaml:"authentication"`
+	Servicemesh    ComponentStatus
+	Openpitrix     ComponentStatus
+	Notification   ComponentStatus
+	NetworkPolicy  ComponentStatus
+	MetricsServer  ComponentStatus
+	Logging        ComponentStatus
+	Events         ComponentStatus
+	DevOps         ComponentStatus
+	Auditing       ComponentStatus
+	Alerting       ComponentStatus
+	Multicluster   Multicluster `yaml:"multicluster"`
+}
+
+// Authentication represents the Authentication of KubeSphere
+type Authentication struct {
+	JwtSecret string `yaml:"jwtSecret"`
 }
 
 // ComponentStatus is a common status
