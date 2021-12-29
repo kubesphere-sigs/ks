9667b8af3dfe2587ae18b9a6215112a41b71f45d
mango <xu.weiKyrie@foxmail.com>
2021-12-29 16:40:27 +0800

    fix: install istioctl to uninstall all istio resources.
    Signed-off-by: mango <xu.weiKyrie@foxmail.com>

--- kubectl-plugin/uninstall/component/servicemesh.go
+++ kubectl-plugin/uninstall/component/servicemesh.go
@@ -10,13 +10,26 @@ type ServiceMesh struct {
 
 // Uninstall uninstall ServiceMesh
 func (s *ServiceMesh) Uninstall() error {
+	if err := common.ExecCommand("wget", "https://github.com/istio/istio/releases/download/1.12.1/istioctl-1.12.1-linux-amd64.tar.gz"); err != nil {
+		return err
+	}
+	if err := common.ExecCommand("tar", "-xvf", "istioctl-1.12.1-linux-amd64.tar.gz"); err != nil {
+		return err
+	}
+	if err := common.ExecCommand("cp", "istioctl", "/usr/local/bin"); err != nil {
+		return err
+	}
+
+	if err := common.ExecCommand("istioctl", "x", "uninstall", "--purge"); err != nil {
+		return err
+	}
 	if err := common.ExecCommand("kubectl", "-n", "istio-system", "delete", "kiali", "kiali"); err != nil {
 		return err
 	}
-	if err := common.ExecCommand("kubectl", "-n", "istio-system", "delete", "jaeger", "jaeger"); err != nil {
+	if err := common.ExecCommand("helm", "-n", "istio-system", "delete", "kiali-operator"); err != nil {
 		return err
 	}
-	if err := common.ExecCommand("helm", "-n", "istio-system", "delete", "kiali-operator"); err != nil {
+	if err := common.ExecCommand("kubectl", "-n", "istio-system", "delete", "jaeger", "jaeger"); err != nil {
 		return err
 	}
 	if err := common.ExecCommand("helm", "-n", "istio-system", "delete", "jaeger-operator"); err != nil {
